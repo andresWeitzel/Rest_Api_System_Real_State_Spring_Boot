20b185cd49c049fdfffa72813ce455fb5c65314b
Andrés Weitzel <andres96energy@hotmail.com>
2023-08-25 13:12:54 -0300

    Added structure project

---
+++ src/main/java/com/sistema_inmobiliaria/ApiRestSistemaInmobiliariaApplication.java
@@ -0,0 +1,13 @@
+package com.sistema_inmobiliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestSistemaInmobiliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestSistemaInmobiliariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sistema_inmobiliaria/ApiRestSistemaInmobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema_inmobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSistemaInmobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
