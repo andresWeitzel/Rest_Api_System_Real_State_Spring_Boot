4d823a98fa2e9579839fecf77980dfe5db1a6f27
Andrés Weitzel <andres96energy@hotmail.com>
2024-02-25 22:22:12 -0300

    Added first configuration with flyway

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Rest_Api_System_Real_State_Spring_Boot
 Rest Api for Real Estate Management System implemented with Spring Boot, Spring Data JPA, Lombok, Maven, Postman, PostgreSQL, Others.
+
+* [spring-data-jpa-generate-db-schema](https://www.baeldung.com/spring-data-jpa-generate-db-schema)
+* [spring data loading initials records](spring-boot-data-sql-and-schema-sql)
--- src/main/java/com/sistema_inmobiliaria/ApiRestSistemaInmobiliariaApplication.java
+++ src/main/java/com/sistema_inmobiliaria/ApiRestSistemaInmobiliariaApplication.java
@@ -1,13 +1,25 @@
 package com.sistema_inmobiliaria;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+
 
 @SpringBootApplication
+@EnableTransactionManagement
+@SpringBootConfiguration
+@Import({DataSourceAutoConfiguration.class, FlywayAutoConfiguration.class})
 public class ApiRestSistemaInmobiliariaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiRestSistemaInmobiliariaApplication.class, args);
+		
 	}
 
 }
